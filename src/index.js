e9e64cb7492e7d6d9d32ee1b6821120badb9cabf
yitu_wangjixin <seabedfish.x@gmail.com>
2021-06-25 17:34:09 +0800

    modify: 修改组件暴露形式

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // 导入button组件
-import ListTableT from './../packages/ListTable'
+import ListTable from './../packages/ListTable'
 
 // 组件列表
 const components = [
-  ListTableT
+  ListTable
 ]
 
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，那么所有的组件都会被注册
@@ -19,7 +19,7 @@ if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
 
-export const ListTable = ListTableT
+export { ListTable }
 
 export default {
   // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
